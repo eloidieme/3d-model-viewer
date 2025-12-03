8b474f5fbcb095acf115421051581fe55bb0a23e
Eloi Dieme <eloidieme@gmail.com>
2025-12-03 14:16:24 +0100

    closes #1

--- src/Core/Window.cpp
+++ src/Core/Window.cpp
@@ -7,6 +7,28 @@
 #include <GLFW/glfw3.h>
 #include <stdexcept>
 
+static void APIENTRY OpenGLMessageCallback(GLenum source, GLenum type,
+                                           unsigned int id, GLenum severity,
+                                           GLsizei length, const char *message,
+                                           const void *userParam) {
+  switch (severity) {
+  case GL_DEBUG_SEVERITY_HIGH:
+    LOG_CORE_CRITICAL("[OpenGL Debug HIGH] {0}", message);
+    return;
+  case GL_DEBUG_SEVERITY_MEDIUM:
+    LOG_CORE_ERROR("[OpenGL Debug MEDIUM] {0}", message);
+    return;
+  case GL_DEBUG_SEVERITY_LOW:
+    LOG_CORE_WARN("[OpenGL Debug LOW] {0}", message);
+    return;
+  case GL_DEBUG_SEVERITY_NOTIFICATION:
+    LOG_CORE_TRACE("[OpenGL Debug NOTIFICATION] {0}", message);
+    return;
+  }
+
+  LOG_CORE_INFO("[OpenGL Debug UNKNOWN] {0}", message);
+}
+
 Window::Window(const WindowConfig &config) {
   m_data.Title = config.Title;
   m_data.Width = config.Width;
@@ -28,6 +50,26 @@ Window::Window(const WindowConfig &config) {
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
     throw std::runtime_error("Failed to initialize GLAD");
   }
+
+  LOG_CORE_INFO("OpenGL Info:");
+  LOG_CORE_INFO("  Vendor: {0}", (const char *)glGetString(GL_VENDOR));
+  LOG_CORE_INFO("  Renderer: {0}", (const char *)glGetString(GL_RENDERER));
+  LOG_CORE_INFO("  Version: {0}", (const char *)glGetString(GL_VERSION));
+
+#ifdef DEBUG
+  int flags;
+  glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+  if (flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE,
+                          GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, GL_FALSE);
+
+    LOG_CORE_INFO("OpenGL Debug Output Enabled");
+  }
+#endif
 
   glfwSetFramebufferSizeCallback(
       m_nativeHandle, [](GLFWwindow *window, int width, int height) {
@@ -112,10 +154,14 @@ Window::Window(const WindowConfig &config) {
 Window::~Window() { glfwDestroyWindow(m_nativeHandle); };
 
 void Window::init() {
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+#ifdef DEBUG
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+#endif
 }
 
 void Window::onUpdate() {
